205a5a3b9c119ce572c24dd395cc3153b3da4454
Jan Biasi <biasijan@gmail.com>
2019-07-16 02:16:57 +0200

    feat: improve tests and quality

--- test/Packer.test.ts
+++ test/Packer.test.ts
@@ -1,30 +1,37 @@
 import { resolve, join } from 'path';
 import { Packer } from '../src/Packer';
-import { fs } from '../src/utils';
-import { HookPhase, IAnalytics } from '../src/types';
-import { create } from 'istanbul-reports';
+import { fs, rimraf } from '../src/utils';
+import { HookPhase, IAnalytics, IAnalyticsWithIntegrity } from '../src/types';
 
 // hack to not provide this as option.cwd in packer ctor
 const BASIC_CWD = resolve(__dirname, 'fixtures/basic');
-const cyclical_CWD = resolve(__dirname, 'fixtures/cyclical');
+const CYCLICAL_CWD = resolve(__dirname, 'fixtures/cyclical');
+const MULTITREE_CWD = resolve(__dirname, 'fixtures/multitree');
+const DUPLICATES_CWD = resolve(__dirname, 'fixtures/duplicates');
+const INVALID_CWD = resolve(__dirname, 'fixtures/duplicates');
 
-const TEMP = resolve(__dirname, 'temp');
+const createTestPackerFor = (cwd: string, source: string, hooks?: Packer['hooks']) =>
+	new Packer({
+		cwd,
+		source,
+		target: resolve(cwd, 'temp'),
+		hooks
+	})
 
 const createTestPackerForBasic = (hooks?: Packer['hooks']) =>
-	new Packer({
-		cwd: BASIC_CWD,
-		source: 'packages/main',
-		target: TEMP,
-		hooks
-	});
+	createTestPackerFor(BASIC_CWD, 'packages/main', hooks);
 
 const createTestPackerForcyclical = (hooks?: Packer['hooks']) =>
-	new Packer({
-		cwd: cyclical_CWD,
-		source: 'packages/a',
-		target: TEMP,
-		hooks
-	});
+	createTestPackerFor(CYCLICAL_CWD, 'packages/a', hooks);
+
+const createTestPackerForMultitree = (hooks?: Packer['hooks']) =>
+	createTestPackerFor(MULTITREE_CWD, 'packages/c', hooks);
+
+const createTestPackerForDuplicates = (hooks?: Packer['hooks']) =>
+	createTestPackerFor(DUPLICATES_CWD, 'packages/main', hooks);
+
+const createTestPackerToGalaxy = (hooks?: Packer['hooks']) =>
+	createTestPackerFor(INVALID_CWD, 'packages/does-not-exist', hooks);
 
 const analyticsToSnapshot = (analytics: IAnalytics) => {
 	return {
@@ -43,11 +50,28 @@ const analyticsToSnapshot = (analytics: IAnalytics) => {
 
 describe('Packer', () => {
 	describe('Lerna', () => {
+		it('should fail on not found validation', async () => {
+			const packer = createTestPackerToGalaxy();
+			const willFailDueNotFound = async () => {
+				try {
+					await packer.validate();
+				} catch (err) {
+					throw new Error(err);
+				}
+			};
+
+			await expect(willFailDueNotFound()).rejects.toThrow();
+
+			try {
+				await willFailDueNotFound();
+			} catch (err) {
+				expect(`${err}`.indexOf('Missing sources, please check if') > -1);
+			}
+		});
 		it('should aggregate any analytics', async () => {
 			const packer = createTestPackerForBasic();
 			const analytics = await packer.analyze();
 			expect(analytics).toBeDefined();
-			console.log(JSON.stringify(analytics, null, 2));
 			expect(analyticsToSnapshot(analytics)).toMatchSnapshot();
 		});
 
@@ -56,13 +80,13 @@ describe('Packer', () => {
 			const analytics = await packer.analyze();
 			expect(analyticsToSnapshot(analytics)).toMatchSnapshot();
 			const validateAnalyticsOutput = () => {
-				const contents = fs.readFileSync(resolve(TEMP, 'monopacker.analytics.json'), 'utf8');
+				const contents = fs.readFileSync(resolve(BASIC_CWD, 'temp', 'monopacker.analytics.json'), 'utf8');
 				expect(contents).toBeDefined();
 			};
 			expect(validateAnalyticsOutput).not.toThrow();
 			validateAnalyticsOutput();
 			try {
-				const contents = fs.readFileSync(resolve(TEMP, 'monopacker.analytics.json'), 'utf8');
+				const contents = fs.readFileSync(resolve(BASIC_CWD, 'temp', 'monopacker.analytics.json'), 'utf8');
 				expect(contents).toBeDefined();
 				const deserialized = JSON.parse(contents);
 				expect(deserialized).toBeTruthy();
@@ -72,13 +96,13 @@ describe('Packer', () => {
 				expect(deserialized.dependencies.peer).toBeDefined();
 				expect(deserialized.dependencies.internal.length).toEqual(1);
 				expect(deserialized.dependencies.peer.smallest).toBeDefined();
-			} catch (err) {}
+			} catch (err) { }
 		});
 
 		it('should generate an artificial package correctly', async () => {
 			const packer = createTestPackerForBasic();
 			await packer.pack();
-			const generatedPkg = require(join(TEMP, 'package.json'));
+			const generatedPkg = require(join(BASIC_CWD, 'temp', 'package.json'));
 			expect(generatedPkg).toBeTruthy();
 			expect(generatedPkg).toMatchSnapshot();
 			expect(generatedPkg.name).toEqual('@fixture/main-packed');
@@ -93,7 +117,7 @@ describe('Packer', () => {
 		it('should copy the source files correctly', async () => {
 			const packer = createTestPackerForBasic();
 			await packer.pack();
-			const contents = require(join(TEMP, 'src', 'test.js'));
+			const contents = require(join(BASIC_CWD, 'temp', 'src', 'test.js'));
 			expect(contents).toEqual('Hello world');
 		});
 
@@ -119,16 +143,16 @@ describe('Packer', () => {
 			const packer = createTestPackerForBasic();
 			await packer.pack();
 
-			const debugExists = await fs.pathExists(resolve(TEMP, 'node_modules', 'debug'));
+			const debugExists = await fs.pathExists(resolve(BASIC_CWD, 'temp', 'node_modules', 'debug'));
 			expect(debugExists).toBe(true);
 
-			const msExists = await fs.pathExists(resolve(TEMP, 'node_modules', 'ms'));
+			const msExists = await fs.pathExists(resolve(BASIC_CWD, 'temp', 'node_modules', 'ms'));
 			expect(msExists).toBe(true);
 
-			const smallestExists = await fs.pathExists(resolve(TEMP, 'node_modules', 'smallest'));
+			const smallestExists = await fs.pathExists(resolve(BASIC_CWD, 'temp', 'node_modules', 'smallest'));
 			expect(smallestExists).toBe(true);
 
-			const arrayExists = await fs.pathExists(resolve(TEMP, 'node_modules', 'to-array'));
+			const arrayExists = await fs.pathExists(resolve(BASIC_CWD, 'temp', 'node_modules', 'to-array'));
 			expect(arrayExists).toBe(true);
 		});
 
@@ -136,13 +160,13 @@ describe('Packer', () => {
 			const packer = createTestPackerForBasic();
 			await packer.pack();
 
-			const baseFolderExists = await fs.pathExists(resolve(TEMP, 'node_modules', '@fixture'));
+			const baseFolderExists = await fs.pathExists(resolve(BASIC_CWD, 'temp', 'node_modules', '@fixture'));
 			expect(baseFolderExists).toBe(true);
 
-			const subModuleExists = await fs.pathExists(resolve(TEMP, 'node_modules', '@fixture', 'sub'));
+			const subModuleExists = await fs.pathExists(resolve(BASIC_CWD, 'temp', 'node_modules', '@fixture', 'sub'));
 			expect(subModuleExists).toBe(true);
 
-			expect(require(resolve(TEMP, 'node_modules', '@fixture', 'sub', 'src', 'index.js'))).toEqual(
+			expect(require(resolve(BASIC_CWD, 'temp', 'node_modules', '@fixture', 'sub', 'src', 'index.js'))).toEqual(
 				'Hello from sub'
 			);
 		});
@@ -150,7 +174,7 @@ describe('Packer', () => {
 		it('should generate correct metadata for the packed bundle', async () => {
 			const packer = createTestPackerForBasic();
 			await packer.pack();
-			const generatedPkg = require(join(TEMP, 'package.json'));
+			const generatedPkg = require(join(BASIC_CWD, 'temp', 'package.json'));
 			expect(generatedPkg).toBeDefined();
 			expect(generatedPkg.monopacker).toBeDefined();
 			expect(generatedPkg.monopacker).toMatchSnapshot();
@@ -159,7 +183,7 @@ describe('Packer', () => {
 		it('should generate a hash for the packed bundle', async () => {
 			const packer = createTestPackerForBasic();
 			await packer.pack();
-			const generatedPkg = require(join(TEMP, 'package.json'));
+			const generatedPkg = require(join(BASIC_CWD, 'temp', 'package.json'));
 			expect(generatedPkg).toBeDefined();
 			expect(generatedPkg.monopacker).toBeDefined();
 			expect(generatedPkg.monopacker.hash).toBeDefined();
@@ -168,7 +192,7 @@ describe('Packer', () => {
 		});
 
 		describe('cyclical dependencies', () => {
-			it.only('should detect endless cycles in dependencies', async () => {
+			it('should detect endless cycles in dependencies', async () => {
 				const packer = createTestPackerForcyclical();
 				const willFailDueCyclical = async () => {
 					try {
@@ -183,7 +207,7 @@ describe('Packer', () => {
 				);
 			});
 
-			it.only('should abort packing if detecting cyclical dependencies', async () => {
+			it('should abort packing if detecting cyclical dependencies', async () => {
 				const fakePackedHook = jest.fn(() => Promise.resolve());
 				const packer = createTestPackerForcyclical({
 					[HookPhase.PACKED]: [fakePackedHook]
@@ -202,5 +226,76 @@ describe('Packer', () => {
 				expect(fakePackedHook).toHaveBeenCalledTimes(0);
 			});
 		});
+
+		describe('multitree dependencies', () => {
+			it('should analyze multitree dependencies correctly', async () => {
+				const packer = createTestPackerForMultitree();
+				const analytics = await packer.analyze(false);
+				expect(analyticsToSnapshot(analytics)).toMatchSnapshot();
+				expect(analytics.graph).toStrictEqual({
+					"@fixture/multitree-a": {
+						smallest: "1.0.1",
+					},
+					"@fixture/multitree-ab": {},
+					"@fixture/multitree-b": {
+						ms: "2.1.2",
+					},
+					"@fixture/multitree-c": {
+						external: {
+							ms: "2.1.2",
+							smallest: "1.0.1",
+						},
+						internal: {
+							"@fixture/multitree-a": "1.0.0",
+							"@fixture/multitree-ab": "1.0.0",
+							"@fixture/multitree-b": "1.0.0",
+						},
+					},
+				})
+			});
+
+			it('should pack multitree apps correctly', async () => {
+				const packer = createTestPackerForMultitree();
+				const analytics = await packer.analyze();
+				await packer.pack();
+				const generatedPkg = require(join(MULTITREE_CWD, 'temp', 'package.json'));
+
+				expect(generatedPkg).toStrictEqual({
+					name: "@fixture/multitree-c-packed",
+					version: "1.0.0",
+					description: "",
+					monopacker: {
+						hash: (analytics as IAnalyticsWithIntegrity).integrity,
+						version: "1",
+						linked: {
+							"@fixture/multitree-ab": "1.0.0",
+							"@fixture/multitree-a": "1.0.0",
+							"@fixture/multitree-b": "1.0.0"
+						}
+					},
+					dependencies: {
+						smallest: "1.0.1",
+						ms: "2.1.2"
+					}
+				});
+			});
+		});
+
+		describe('multiple identical package names', () => {
+			it('should abort if multiple packages have the same name', async () => {
+				const packer = createTestPackerForDuplicates();
+				const willFailDueDuplicates = async () => {
+					try {
+						await packer.validate();
+					} catch (err) {
+						throw new Error(err);
+					}
+				};
+
+				await expect(willFailDueDuplicates()).rejects.toThrow(
+					'Duplicate package names found: @fixture/duplicate-a'
+				);
+			})
+		})
 	});
 });
